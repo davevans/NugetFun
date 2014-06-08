19375307c7105b12954106e0784a4ae9652ce346
Dav Evans <dav@dav-evans.com>
2014-06-08 16:39:30 +1000

    Add assemblyInfo sections

--- NuGetFun.LibraryB/NuGetFun.LibraryB/Properties/AssemblyInfo.cs
+++ NuGetFun.LibraryB/NuGetFun.LibraryB/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NuGetFun.LibraryB")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This is LibraryB")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DavCorp")]
 [assembly: AssemblyProduct("NuGetFun.LibraryB")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
